370c3a8c5898759e8d95451ba9ff2443c59e1c00
Srinivas Kaza <srinivas@kaza.io>
2017-12-25 20:00:41 -0600

    Somewhat more intelligent model

--- network/upsample.py
+++ network/upsample.py
@@ -20,18 +20,11 @@ from sklearn.pipeline import Pipeline
 
 
 def keras_format_data(data):
-    # return np.rollaxis(data, 3, 1).astype(float32)
     return data.astype(float32)
 
 
 def load_data(filename):
     with h5py.File(filename, 'r') as f:
-        '''X_train = np.rollaxis(f['train']['features'][:], 3, 1).astype(float32)
-        Y_train = np.rollaxis(f['train']['predictions']
-                              [:], 3, 1).astype(float32)
-        X_test = np.rollaxis(f['train']['features'][:], 3, 1).astype(float32)
-        Y_test = np.rollaxis(f['test']['predictions'][:], 3, 1).astype(float32)'''
-
         X_train = keras_format_data(f['train']['features'][:])
         X_test = keras_format_data(f['test']['features'][:])
         Y_train = keras_format_data(f['train']['predictions'][:])
@@ -46,18 +39,19 @@ def create_convnet_model():
     model.add(Conv2D(96, (3, 3), padding='valid',
                      data_format="channels_last",
                      activation='relu', input_shape=(7, 7, 4)))
+    model.add(GaussianDropout(0.1))
     model.add(Conv2D(128, (3, 3), padding='valid',
                      data_format="channels_last",
-                     activation='relu', input_shape=(5, 5, 96)))
-    model.add(Conv2D(256, (2, 2), padding='valid',
+                     activation='relu', input_shape=(5, 5, 128)))
+    model.add(Conv2D(128, (2, 2), padding='valid',
                      data_format="channels_last",
                      activation='relu', input_shape=(3, 3, 128)))
-    model.add(Conv2D(256, (1, 1), padding='same',
+    model.add(Conv2D(128, (1, 1), padding='same',
                      data_format="channels_last",
-                     activation='relu', input_shape=(2, 2, 256)))
-    model.add(Flatten(input_shape=(2, 2, 256)))
-    model.add(Dense(128, activation='relu', input_shape=(2 * 2 * 256,)))
-    model.add(Dense(2 * 2 * 3, activation='relu', input_shape=(128,)))
+                     activation='relu', input_shape=(2, 2, 128)))
+    model.add(Flatten(input_shape=(2, 2, 128)))
+    model.add(Dense(500, activation='relu', input_shape=(2 * 2 * 128,)))
+    model.add(Dense(2 * 2 * 3, activation='linear', input_shape=(128,)))
     model.add(Reshape((2, 2, 3), input_shape=(2 * 2 * 3,)))
 
     return model
@@ -68,7 +62,11 @@ def create_simple_model():
 
     model.add(Flatten(input_shape=(7, 7, 4)))
     model.add(Dense(1000, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.1))
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dropout(0.1))
     model.add(Dense(2 * 2 * 3, activation='relu'))
     model.add(Reshape((2, 2, 3)))
 
@@ -77,10 +75,10 @@ def create_simple_model():
 
 def train_model(model, data):
     X_train, Y_train, X_test, Y_test = data
-    model.compile(loss='mean_squared_error',
+    model.compile(loss='mean_absolute_error',
                   optimizer='adam', metrics=['accuracy'])
     model.fit(X_train, Y_train, batch_size=256,
-              epochs=10, verbose=1, validation_split=0.3)
+              epochs=10, verbose=1, validation_split=0.1)
     score = model.evaluate(X_test, Y_test, verbose=1)
     print(score)
 
